c271640b08c88f9b5a2fcf44c7ff36e6929d6e6b
Leandro Di Napoli <43824701+ldinapoli@users.noreply.github.com>
2022-02-19 19:17:26 -0300

    movement improved

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,16 @@ public class Player : MonoBehaviour
     private GameObject cam1;
     [SerializeField]
     private GameObject cam2;
+    [SerializeField]
+    private Transform mainCameraTransform;
+
+    Vector3 _playerVelocity;
+    bool _groundedPlayer;
+    CharacterController _controller;
+    float _jumpHeight = 1.0f;
+    float _gravityValue = -9.81f;
+    float _turnSmoothTime = 0.1f;
+    float _turnSmoothVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -22,30 +32,44 @@ public class Player : MonoBehaviour
         HealPlayer(5);
         Debug.Log("player healed current health: " + health);
 
+        _controller = gameObject.GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        _groundedPlayer = _controller.isGrounded;
+        if (_groundedPlayer && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = 0f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _controller.Move(moveDir.normalized * Time.deltaTime * movementSpeed);
+        }   
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && _groundedPlayer)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+        }
+
         if (Input.GetKeyDown(KeyCode.N))
         {
             cam1.SetActive(!cam1.activeSelf);
             cam2.SetActive(!cam2.activeSelf);
         }
-    }
-
-    void MoveToDirectionWithSpeed(Vector3 direction, int movementSpeed)
-    {
-        transform.position += direction * movementSpeed * Time.deltaTime;
-    }
-
-    void Move()
-    {
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(hor, 0, ver) * movementSpeed * Time.deltaTime);
     }
 
     void DamagePlayer(int damageDealt)
